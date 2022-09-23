dfb2fd97b9439699e61f326aa6bd5534c5e5c250
Renata Yakobov <rinat.yuri@gmail.com>
2022-09-22 22:20:39 -0400

    Working version

--- calculator.js
+++ calculator.js
@@ -17,10 +17,157 @@
     // - can SQUARE ROOT
     // - can handle three or more numbers
 
-const firstNumber = prompt("First number: ");
-const operator = prompt("Operator: ");
-const secondNumber = prompt("Second number: ");
+// const firstNumber = Number(prompt("First number: "));
+// const operator = prompt("Operator: ");
+// const secondNumber = Number(prompt("Second number: "));
 
-alert(`${firstNumber} ${operator} ${secondNumber}`);
+// console.log (typeof firstNumber);
+// console.log (typeof secondNumber);
+
+const supportedOperators = ["+", "-", "*", "/" ];
+// const firstNumber = 5;
+// const operator = "/";
+// const secondNumber = 6;
 
 // CODE GOES HERE
+//get input text
+let firstNumberEntered = false;
+let secondNumberEntered = false;
+let operatorEntered = false;
+let equalEntered = false;
+let firstNumberValue = "";
+let secondNumberValue = "";
+let globalOperatorValue = "";
+let result = "";
+
+let displayInput = document.querySelector (".result__display__input");
+console.log (displayInput.value);
+
+//get result text
+const displayResult = document.querySelector (".result__display__result");
+
+//get numbers buttons 
+const clickedNumber = document.querySelectorAll(".buttons__number");
+
+//get operators buttons 
+const operators = document.querySelectorAll(".buttons__operator");
+
+//get = button 
+const equal = document.querySelector(".buttons__calc");
+console.log (equal);
+
+//get reset button 
+const reset = document.querySelector(".buttons__reset");
+console.log(reset);
+
+//function to enable operators
+const enableOperators = () => {
+    operators.forEach ((operator) => {
+        operator.disabled = false;
+    } );
+}
+//function to disable operators
+const disableOperators = () => {
+    operators.forEach ((operator) => {
+        operator.disabled = true;
+    } );
+}
+
+//function to enable numbers
+const enableNumbers = () => {
+    clickedNumber.forEach ((number) => {
+        number.disabled = false;
+    } );
+}
+//function to disable numbers
+const disableNumbers = () => {
+    clickedNumber.forEach ((number) => {
+        number.disabled = true;
+    } );
+}
+
+// add clicked numbers to input section of the calculator and enable operators
+clickedNumber.forEach((number) => {
+    number.addEventListener ("click", (event) => {
+        const tempNumber = number.innerHTML;
+        if (!operatorEntered) enableOperators(); // enable operators if entering first number
+        if (operatorEntered && !secondNumberEntered) {
+            displayInput.value = displayInput.value + " " + tempNumber;
+        } else displayInput.value  = displayInput.value + tempNumber;
+               
+        if (firstNumberEntered) {
+            secondNumberEntered = true ;
+            secondNumberValue += tempNumber; //keep adding to the second number value for the calculation function use
+        } else firstNumberValue += tempNumber; // keep adding to the first number value for the calculation function use
+           
+        // console.log (firstNumberValue + " " + secondNumberValue);
+    })
+}
+)
+// add operator to the input section of the calculator
+operators.forEach((operator) => {
+    operator.addEventListener ("click", (event) => {
+        let operatorValue = operator.innerHTML;
+        displayInput.value = displayInput.value + " " + operatorValue;
+        operatorEntered = true;
+        firstNumberEntered = true;
+        globalOperatorValue = operatorValue;
+        disableOperators();
+    })
+})
+
+// add " = " to the input section of the calculator
+
+equal.addEventListener ("click", (event) => {
+    if (!equalEntered) {
+        let equalValue = equal.innerHTML;
+        displayInput.value = displayInput.value + " " + equalValue;
+        result = calculate (Number(firstNumberValue), Number(secondNumberValue), globalOperatorValue );
+        displayResult.value = result;
+        console.log (result);
+        equalEntered = true;
+        disableOperators();
+        disableNumbers();
+    }        
+})
+
+reset.addEventListener ("click", (event) => {
+    firstNumberEntered = false;
+    secondNumberEntered = false;
+    operatorEntered = false;
+    equalEntered = false;
+    firstNumberValue = "";
+    secondNumberValue = "";
+    globalOperatorValue = "";
+    result = "";
+    displayInput.value = ""
+    displayResult.value = "";
+    disableOperators();
+    enableNumbers();          
+})
+
+const calculate = (firstNumber, secondNumber, operator) => {
+    if (!supportedOperators.includes(operator)) {
+        return "Operation is not supported" ;
+    }
+    else if (typeof firstNumber != "number" || typeof secondNumber != "number" ) {
+        return "Please provide two numbers" ;
+    }
+    else {
+        switch (operator) {
+            case "+": 
+                return firstNumber + secondNumber ;
+                break;
+            case "-": 
+                return firstNumber - secondNumber ;
+                break;
+            case "*": 
+                return firstNumber * secondNumber ;
+                break;
+            case "/": 
+                return firstNumber / secondNumber ;
+                break;    
+        }
+        
+    }
+}
